420df6dc855b43b77923cd60debfd2b69a29741b
Brandon Drydyk <drydyk@Brandons-MacBook-Pro.local>
2018-04-08 21:25:36 -0700

    int

--- pyhap/accessories/BMP180.py
+++ pyhap/accessories/BMP180.py
@@ -1,10 +1,13 @@
 # An Accessory for the BMP180 sensor.
 # This assumes the bmp180 module is in a package called sensors.
 # Assume you have a bmp module with BMP180 class with read() method.
-from sensors.bmp180 import BMP180 as sensor
+from Sensors import BMP085
 
 from pyhap.accessory import Accessory, Category
 import pyhap.loader as loader
+
+import smbus
+import RPi.GPIO as GPIO
 
 
 class BMP180(Accessory):
@@ -17,7 +20,7 @@ class BMP180(Accessory):
         self.temp_char = self.get_service("TemperatureSensor")\
                              .get_characteristic("CurrentTemperature")
 
-        self.sensor = sensor()
+        self.sensor = BMP085(0x77, 1)
 
     def _set_services(self):
         super(BMP180, self)._set_services()
@@ -31,9 +34,9 @@ class BMP180(Accessory):
 
     def __setstate__(self, state):
         self.__dict__.update(state)
-        self.sensor = sensor()
+        self.sensor = BMP085(0x77, 1)
 
     def run(self):
         while not self.run_sentinel.wait(30):
-            temp, _pressure = self.sensor.read()
+            temp  = self.sensor.readTemperature()
             self.temp_char.set_value(temp)
